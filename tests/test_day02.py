f3bd0f965f641529d0591d60ad3d951fe695071b
Surya Dutta <surya.dutta@spothero.com>
2023-12-05 13:04:03 -0600

    more

--- tests/test_day02.py
+++ tests/test_day02.py
@@ -9,6 +9,14 @@ from src.day02 import (
     run_part_b,
 )
 
+TEST_LINES = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
 
 @pytest.mark.parametrize(
     "line_input,expected",
@@ -29,27 +37,11 @@ def test_parse_game_info(line_input: str, expected: GameInfo):
 
 @patch("src.day02.get_data")
 def test_run_part_a(mock_get_data):
-    mock_get_data.return_value = [
-        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-    ]
-
-    answer = run_part_a()
-    assert answer == str(8)
+    mock_get_data.return_value = TEST_LINES
+    assert run_part_a() == str(8)
 
 
 @patch("src.day02.get_data")
 def test_run_part_b(mock_get_data):
-    mock_get_data.return_value = [
-        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-    ]
-
-    answer = run_part_b()
-    assert answer == "2286"
+    mock_get_data.return_value = TEST_LINES
+    assert run_part_b() == str(2286)
