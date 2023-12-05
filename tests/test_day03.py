e2d3963c11a5366839d05064d93fadeb27016fa5
Surya Dutta <surya.dutta@spothero.com>
2023-12-05 13:02:22 -0600

    cleanup[

--- tests/test_day03.py
+++ tests/test_day03.py
@@ -5,40 +5,27 @@ from src.day03 import (
     run_part_b,
 )
 
+TEST_LINES = [
+    "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "617*......",
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "...$.*....",
+    ".664.598..",
+]
+
 
 @patch("src.day03.get_data")
 def test_run_part_a(mock_get_data):
-    mock_get_data.return_value = [
-        "467..114..",
-        "...*......",
-        "..35..633.",
-        "......#...",
-        "617*......",
-        ".....+.58.",
-        "..592.....",
-        "......755.",
-        "...$.*....",
-        ".664.598..",
-    ]
-
-    answer = run_part_a()
-    assert answer == str(4361)
+    mock_get_data.return_value = TEST_LINES
+    assert run_part_a() == str(4361)
 
 
 @patch("src.day03.get_data")
 def test_run_part_b(mock_get_data):
-    mock_get_data.return_value = [
-        "467..114..",
-        "...*......",
-        "..35..633.",
-        "......#...",
-        "617*......",
-        ".....+.58.",
-        "..592.....",
-        "......755.",
-        "...$.*....",
-        ".664.598..",
-    ]
-
-    answer = run_part_b()
-    assert answer == str(467835)
+    mock_get_data.return_value = TEST_LINES
+    assert run_part_b() == str(467835)
