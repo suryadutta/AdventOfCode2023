4f55ef80a91cc139daa5ebd3d574d214639e93c6
Surya Dutta <surya@suryadutta.com>
2023-12-05 23:00:54 -0600

    Day 5 Part A (#8)

---
+++ tests/test_day05.py
@@ -0,0 +1,51 @@
+from unittest.mock import patch
+
+from src.day05 import (
+    run_part_a,
+)
+
+TEST_LINES = [
+    "seeds: 79 14 55 13",
+    "",
+    "seed-to-soil map:",
+    "50 98 2",
+    "52 50 48",
+    "",
+    "soil-to-fertilizer map:",
+    "0 15 37",
+    "37 52 2",
+    "39 0 15",
+    "",
+    "fertilizer-to-water map:",
+    "49 53 8",
+    "0 11 42",
+    "42 0 7",
+    "57 7 4",
+    "",
+    "water-to-light map:",
+    "88 18 7",
+    "18 25 70",
+    "",
+    "light-to-temperature map:",
+    "45 77 23",
+    "81 45 19",
+    "68 64 13",
+    "",
+    "temperature-to-humidity map:",
+    "0 69 1",
+    "1 0 69",
+    "",
+    "humidity-to-location map:",
+    "60 56 37",
+    "56 93 4",
+]
+
+
+@patch("src.day05.get_data")
+def test_run_part_a(mock_get_data):
+    mock_get_data.return_value = TEST_LINES
+    assert run_part_a() == str(35)
+
+
+def test_run_part_b():
+    assert True
