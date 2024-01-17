6c02f5e781a0a11bfae8e85d048492bac8487ece
Surya Dutta <surya.dutta@spothero.com>
2024-01-16 22:14:10 -0600

    more for day 7

--- src/day07.py
+++ src/day07.py
@@ -1,45 +1,82 @@
+from dataclasses import dataclass
+from enum import Enum
+
 from src._logger import LOGGER
 from src.utils import get_data
 
-from collections import Counter
-from dataclasses import dataclass
+CARDS_BY_STRENGTH = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
-CARDS_BY_STRENGTH = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+class CardHandType(Enum):
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    FULL_HOUSE = 5
+    FOUR_OF_A_KIND = 6
+    FIVE_OF_A_KIND = 7
+
 
 @dataclass
 class CardHand:
-    hand: str
+    cards: list[str]
+    bid: int
 
     @property
-    def counter(self):
-        return Counter(self.hand)
+    def hand_type(self) -> CardHandType:
+        unique_cards = set(self.cards)
+
+        match len(unique_cards):
+            case 1:
+                return CardHandType.FIVE_OF_A_KIND
+
+            case 2:
+                counts = [self.cards.count(value) for value in unique_cards]
+
+                if 4 in counts:
+                    return CardHandType.FOUR_OF_A_KIND
+                else:
+                    return CardHandType.FULL_HOUSE
+
+            case 3:
+                counts = [self.cards.count(value) for value in unique_cards]
+
+                # 3 of a kind or 2 pair
+
+                if 3 in counts:
+                    return CardHandType.THREE_OF_A_KIND
+                else:
+                    return CardHandType.TWO_PAIR
+
+            case 4:
+                return CardHandType.ONE_PAIR
+            case 5:
+                return CardHandType.HIGH_CARD
+
+        return CardHandType.HIGH_CARD
 
     @property
-    def distinct_card_count(self):
-        return len(self.counter)
-
-    @property
-    def sorted_card_strengths(self):
-        strengths = list(map(lambda x: CARDS_BY_STRENGTH.index(x), list(self.counter.keys())))
-        strengths.sort(reverse=True)
-        return strengths
-
+    def score(self) -> int:
+        return self.hand_type.value * (14**5) + sum(
+            (CARDS_BY_STRENGTH.index(self.cards[i]) + 1) * 14 ** (4 - i)
+            for i in range(5)
+        )
 
 
 def run_part_a() -> str:
     LOGGER.info("Running code for Part A")
 
-    hands = []
-    bids = []
+    hands: list[CardHand] = []
 
     for line in get_data():
         split = line.split(" ")
-
         bid = int(split[1])
-        hand = CardHand(hand=split[0])
-        strengths = hand.sorted_card_strengths
+        cards = list(split[0])
+        hands.append(CardHand(cards=cards, bid=bid))
 
-    return str()
+    hands.sort(key=lambda hand: hand.score, reverse=False)
+    total_bid_score = sum((rank + 1) * hand.bid for rank, hand in enumerate(hands))
+    return str(total_bid_score)
 
 
 def run_part_b() -> str:
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 
-from src.day07 import run_part_a, run_part_b
+import pytest
+
+from src.day07 import CardHand, CardHandType, run_part_a, run_part_b
 
 TEST_LINES = [
     "32T3K 765",
@@ -9,6 +11,41 @@ TEST_LINES = [
     "KTJJT 220",
     "QQQJA 483",
 ]
+
+
+@pytest.mark.parametrize(
+    "input_hand, expected",
+    [
+        (["2", "3", "4", "5", "6"], CardHandType.HIGH_CARD),
+        (["2", "3", "4", "6", "6"], CardHandType.ONE_PAIR),
+        (["2", "4", "4", "6", "6"], CardHandType.TWO_PAIR),
+        (["2", "4", "6", "6", "6"], CardHandType.THREE_OF_A_KIND),
+        (["2", "2", "6", "6", "6"], CardHandType.FULL_HOUSE),
+        (["2", "6", "6", "6", "6"], CardHandType.FOUR_OF_A_KIND),
+        (["6", "6", "6", "6", "6"], CardHandType.FIVE_OF_A_KIND),
+    ],
+)
+def test_card_hand_type(input_hand: list[str], expected: CardHandType):
+    card_hand = CardHand(cards=input_hand, bid=10)
+    assert card_hand.hand_type == expected
+
+
+@pytest.mark.parametrize(
+    "input_hand, expected",
+    [
+        (
+            ["2", "3", "4", "5", "6"],
+            1 * (14**5) + 1 * (14**4) + 2 * (14**3) + 3 * (14**2) + 4 * (14**1) + 5,
+        ),
+        (
+            ["6", "6", "6", "6", "6"],
+            7 * (14**5) + 5 * (14**4) + 5 * (14**3) + 5 * (14**2) + 5 * (14**1) + 5,
+        ),
+    ],
+)
+def test_card_hand_score(input_hand: list[str], expected: int):
+    card_hand = CardHand(cards=input_hand, bid=10)
+    assert card_hand.score == expected
 
 
 @patch("src.day07.get_data")
